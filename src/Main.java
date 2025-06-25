1c02968473a485991ab1e66d37d6c379ed0c16d8
Nabin Joshi <joshinabin792@gmail.com>
2025-06-25 18:24:19 +0545

    crud

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+// src/Main.java
+
+public class Main {
+    public static void main(String[] args) {
+        StudentManager manager = new StudentManager();
+        manager.start();
+    }
+}
--- src/Student.java
+++ src/Student.java
@@ -1,15 +1,24 @@
 // src/Student.java
 
 public class Student {
+    private int id;
     private String name;
     private int age;
 
-    public Student(String name, int age) {
+    public Student(int id, String name, int age) {
+        this.id = id;
         this.name = name;
         this.age = age;
     }
 
-    public void displayInfo() {
-        System.out.println("Name: " + name + ", Age: " + age);
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public int getAge() { return age; }
+
+    public void setName(String name) { this.name = name; }
+    public void setAge(int age) { this.age = age; }
+
+    public void display() {
+        System.out.println("ID: " + id + " | Name: " + name + " | Age: " + age);
     }
 }

54ecee83c98646661dda29a9afac21341db87f8a
Nabin Joshi <joshinabin792@gmail.com>
2025-06-25 17:45:53 +0545

    hello

---
+++ src/Student.java
@@ -0,0 +1,15 @@
+// src/Student.java
+
+public class Student {
+    private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void displayInfo() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+}

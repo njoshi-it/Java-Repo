05b7e31937649e167e170eb760f1b48017b22c5d
Nabin Joshi <joshinabin792@gmail.com>
2025-06-25 18:10:43 +0545

    hello

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main {
+public class Hello {
     public static void main(String[] args) {
         ArrayList<Student> students = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
